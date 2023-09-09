b17c97fec26ca28a4d95ca9a8480116e67e6e9d5
Nethmi-24 <batawalann.20@uom.lk>
2023-09-09 14:34:44 +0530

    navigate back to dashboard from item panel

--- src/main/java/lk/ijse/stockmanage/controller/ItemformController.java
+++ src/main/java/lk/ijse/stockmanage/controller/ItemformController.java
@@ -1,4 +1,22 @@
 package lk.ijse.stockmanage.controller;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
 public class ItemformController {
+    public AnchorPane root;
+
+    public void btnBackOnAction(ActionEvent actionEvent) throws IOException {
+        Parent rootNode= FXMLLoader.load(this.getClass().getResource("/View/dashboard_form.fxml"));
+        Stage stage=(Stage)this.root.getScene().getWindow();
+        Scene scene=new Scene(rootNode);
+        stage.setScene(scene);
+        stage.setTitle("Dashboard");
+    }
 }
